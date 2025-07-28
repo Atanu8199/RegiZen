a8d7ed513ef50bade989fa5965bdb266709b5c10
Atanu8199 <84720455+Atanu8199@users.noreply.github.com>
2025-07-28 20:58:41 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,19 @@
 module.exports = {
   name: 'messageCreate',
   async execute(message, client) {
-    if (!message.content.startsWith('!')) return;
+    if (!message.content.startsWith('!') || message.author.bot) return;
+
     const args = message.content.slice(1).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     const command = client.commands.get(cmd);
-    if (command) {
-      try {
-        await command.execute(message, args, client);
-      } catch (err) {
-        console.error(err);
-        message.reply('⚠️ There was an error executing that command.');
-      }
+    if (!command) return;
+
+    try {
+      await command.execute(message, args, client);
+    } catch (err) {
+      console.error(err);
+      message.reply('⚠️ There was an error executing that command.');
     }
   }
 };
