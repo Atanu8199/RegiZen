d20c9cb6453ae8851a9c58aa3e76d05315b14715
Atanu8199 <84720455+Atanu8199@users.noreply.github.com>
2025-07-29 14:40:50 +0530

    Update ScrimRegistration.js

--- models/ScrimRegistration.js
+++ models/ScrimRegistration.js
@@ -9,31 +9,22 @@ const scrimRegistrationSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  userId: {
-    type: String,
-    required: true // The user who submitted the form
-  },
   teamName: {
     type: String,
     required: true
   },
-  discordTags: {
-    type: [String], // Array of 4 Discord tags (e.g., ["<@123>", "<@456>", ...])
-    required: true,
-    validate: [arrayLimit, '{PATH} must have exactly 4 players']
-  },
-  slotNumber: {
-    type: Number,
+  players: {
+    type: [String], // Discord tags or IDs (e.g., ["<@123>", "<@456>", ...])
     required: true
   },
-  timestamp: {
+  registeredBy: {
+    type: String, // Discord ID of the person who registered
+    required: true
+  },
+  registeredAt: {
     type: Date,
     default: Date.now
   }
 });
-
-function arrayLimit(val) {
-  return val.length === 4;
-}
 
 module.exports = mongoose.model('ScrimRegistration', scrimRegistrationSchema);
